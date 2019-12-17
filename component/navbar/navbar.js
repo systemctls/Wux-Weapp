05d0839f8f59b0391d843b3006d3d6dd4a0f23c1
mr.jinxm <jinxiaoman@bingdou.tv>
2019-12-17 18:16:55 +0800

    修复nav

--- component/navbar/navbar.js
+++ component/navbar/navbar.js
@@ -1,11 +1,11 @@
-const SysInfo = wx.getSystemInfoSync()
-const MenuRect = wx.getMenuButtonBoundingClientRect()
+const SysInfo = wx.getSystemInfoSync();
+const MenuRect = wx.getMenuButtonBoundingClientRect();
 
 Component({
   properties: {
     text: {
       type: String,
-      value: 'Wechat'
+      value: "Wechat"
     },
     back: {
       type: Boolean,
@@ -21,8 +21,8 @@ Component({
     }
   },
   data: {
-    statusBarHeight: '',
-    navigationBarHeight: ''
+    statusBarHeight: "",
+    navigationBarHeight: ""
   },
   attached() {
     this.initLayout();
@@ -31,24 +31,26 @@ Component({
   methods: {
     initLayout() {
       const statusBarHeight = SysInfo.statusBarHeight;
-      const height = (MenuRect.top - statusBarHeight) * 2 + MenuRect.height
+      const height = (MenuRect.top - statusBarHeight) * 2 + MenuRect.height;
       this.setData({
-        statusBarHeight: statusBarHeight + 'px',
-        navigationBarHeight: `height:${height + statusBarHeight}px;line-height:${height}px;font-size:${SysInfo.fontSizeSetting - 2}pt;`,
-      })
+        statusBarHeight: statusBarHeight + "px",
+        navigationBarHeight: `height:${height +
+          statusBarHeight}px;line-height:${height}px;font-size:${SysInfo.fontSizeSetting -
+          2}pt;`
+      });
     },
-    backHome: function () {
-      console.log('你点击首页')
-      let pages = getCurrentPages()
+    backHome: function() {
+      console.log("你点击首页");
+      let pages = getCurrentPages();
       wx.navigateBack({
         delta: pages.length
-      })
+      });
     },
-    back: function () {
-      console.log('你点击返回')
+    back: function() {
+      console.log("你点击返回");
       wx.navigateBack({
         delta: 1
-      })
+      });
     }
   }
-})
+});
